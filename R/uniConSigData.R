0f09387a2a7d9d50e0a16eb57e4195e332587cf2
Xiaosong Wang Lab <35302520+wangxlab@users.noreply.github.com>
2018-12-06 12:56:48 -0500

    Update uniConSigData.R

--- R/uniConSigData.R
+++ R/uniConSigData.R
@@ -3,7 +3,7 @@
 #' @title pre-calculated penalization factors and molecular concepts coefficients
 #' @name preCal.data
 #' @docType data
-#' @author Xu Chi <xuchi_chisquare@hotmail.com>
+#' @author Xu Chi <wangxlab2018@gmail.com>
 #' @description This file provides the coefficients for uniConSig calculation. <br> The details of the coefficients can be found in the package "uniConSig"
 #' @usage data(preCal.data)
 #' @keywords datasets
@@ -13,9 +13,4 @@
 #' used in uniConSig, the rest are the molecular concepts which are <br>
 #' related to this gene and their coefficients.
 #' @examples
-#' library(uniConSigData)
-#' data(preCal.data)
-#' length(preCal.data)
-#' class(preCal.data)
-#' preCal.data[[1]]
 NULL
